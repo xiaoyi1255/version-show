68c80342fee887faca9ebbcbb19c99b16ca0de25
静ming <1255274497@qq.com>
2025-06-21 00:43:26 +0800

    sssssd

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 echo "🚀 正在执行 update-version.sh ..."
 
-# 1️⃣  判断当前分支为 test 分支; 
+# 1️⃣  判断当前分支为 test 分支; 不是跳过
+# 2️⃣ 判断是否为更新版本号，是跳过
+# 3️⃣ 判断结束，生成版本号
 CURRENT_BRANCH=$(git branch --show-current)
 echo "当前分支: $CURRENT_BRANCH"
-# 如果当前分支不是 test 分支，则终止执行
-# if [ "$CURRENT_BRANCH" != "master" ]; then
-#   echo "⏹️ 当前分支不是 test，终止执行！"
-#   exit 1 
-# fi
-
-# 2️⃣ 判断是否为更新版本号，是终止
 latest_commit_msg=$(git log -1 --pretty=%B)
 echo "最新提交信息: $latest_commit_msg"
 if [ "$CURRENT_BRANCH" == "master" ] && ! echo "$latest_commit_msg" | grep -q "UPDAT_VERSION"; then
-  # 3️⃣ 判断结束，生成版本号
   TIMESTAMP=$(date "+%Y%m%d%H%M%S")
   mkdir -p public/js
 
